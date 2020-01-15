683d3ad1045ba5263d9b70ea8fb2ae4f28c46c27
Cill <prescilla.narain@gmail.com>
2020-01-15 10:28:35 -0500

    Done with the first one again!

---
+++ Cill Narain Hello World Again/Cill Narain Hello World Again/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cill_Narain_Hello_World_Again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cill Narain
+            //1/15/2020
+            //Purpose : Hello World, the basics of programmming, hopefully this should help me with commenting. Also utlizes Console.WriteLine
+            //Using the magical powers of c-w tab X2, Also known as Console.WriteLine, this welcomes the user to the console window!
+            Console.WriteLine("Hello World~");
+            /*This prints out my name!*/
+            Console.WriteLine("Cill Narain!");
+            //Since we need to keep the window up, we use Console.Readline to read a line and by extension, keep the window up.
+            Console.ReadLine();
+
+        }
+    }
+}
