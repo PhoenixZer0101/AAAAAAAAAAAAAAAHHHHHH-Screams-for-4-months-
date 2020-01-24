ddddb8f381c72d6a688ee10be19b724a364eb07a
Cill <prescilla.narain@gmail.com>
2020-01-23 21:07:26 -0500

    Done!

--- Variables and Data Types Again/Variables and Data Types Again/Program.cs
+++ Variables and Data Types Again/Variables and Data Types Again/Program.cs
@@ -32,34 +32,35 @@ namespace Variables_and_Data_Types_Again
             Console.ReadLine();
             ///Second part includes a lot of math
             purchaseShip = 70;
-            Console.WriteLine(name + " purchased a ship for " + purchaseShip + " resources.");
             purchaseUpgrade = 16;
+            Console.WriteLine(name + " purchased a ship for " + purchaseShip + " resources.");
             Console.WriteLine(name + " purchased a weapon upgrade for " + purchaseUpgrade + " resources.");
-            Console.WriteLine(">> Current Resources: " + (currentResources - purchaseShip - purchaseUpgrade));
+            currentResources = currentResources - purchaseShip - purchaseUpgrade;
+            Console.WriteLine(">> Current Resources: " + currentResources);
             Console.ReadLine();
             ///Third part, our resources are nearly gone!
-            currentResources = 14;
             missileNum = 3;
             Console.WriteLine(name + " purchased " + missileNum + " missiles to protect the ship");
-            Console.WriteLine(">> Current Resources: " + (currentResources - missileNum * missileCost));
+            currentResources = currentResources - missileNum * missileCost;
+            Console.WriteLine(">> Current Resources: " + (currentResources));
             Console.ReadLine();
             ///Fourth part, Violence and Chaos!
-            currentResources = 2;
             alienNum = 7;
             Console.WriteLine(name + " obliterated " + alienNum + " aliens and earned " + alienNum * alienKillScore + " resources");
-            Console.WriteLine(">> Current Resources: " + (currentResources + alienNum * alienKillScore));
+            currentResources = currentResources + alienNum * alienKillScore;
+            Console.WriteLine(">> Current Resources: " + currentResources);
             Console.ReadLine();
             ///Fifth part, Yay! More resources!!
-            currentResources = 37;
             stealGoods = 112;
             Console.WriteLine(name + " sold " + stealGoods + " resources worth of stolen goods.");
-            Console.WriteLine(">> Current Resources: " + (currentResources + stealGoods));
+            currentResources = currentResources + stealGoods;
+            Console.WriteLine(">> Current Resources: " + currentResources);
             Console.ReadLine();
             ///Sixth Part, here's to Vacation!
-            currentResources = 149;
             hangarSpace = 20;
             Console.WriteLine(name + " leased a month of hangar space on Planet Xylew for " + hangarSpace + " resources.");
-            Console.WriteLine(">> Current Resources: " + (currentResources - hangarSpace));
+            currentResources = currentResources - hangarSpace;
+            Console.WriteLine(">> Current Resources: " + currentResources);
 
 
 
